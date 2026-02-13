d847711e8c901b598e7902938fa9f79635e250f2
Alef Loresca <lorescajustin@gmail.com>
2026-02-13 22:07:36 +0800

    Fix footer design

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -4,38 +4,29 @@ import './css/footer-styles.css';
 const Footer: React.FC = () => {
     return(
         <section id="footerContainer">
-            <section className="footerSiteDescription">
-                <section id="fullDescription">
-                    <h5>
-                        It focuses exclusively on the core PokeDex structure (designed as data-reference tool for the list of Pokemon. 
-                        This to maintain a clean focus on Pokedex architecture) and interacts with the PokeAPI to provide real-time data 
-                        on over 1,300+ Pokemon, featuring dynamic filtering, searching, and a detailed modal view.
-                    </h5>
+            <img src="./public/pokedex-logo.png" alt="pokedex logo" />
 
-                    <h5>
-                        <small>
-                            &copy; 2026 by Alef Justin Loresca. Pokémon names, images, and related media are trademarks and copyrights of 
-                            Nintendo, Game Freak, and Creatures.. This project is intended under "Fair Use" for educational purposes. 
-                            No copyright infringement is intended
-                        </small>
-                    </h5>
-                </section>
-            </section>
+            <h4>
+                It focuses exclusively on the core PokeDex structure (designed as data-reference tool for the list of Pokemon. 
+                This to maintain a clean focus on Pokedex architecture) and interacts with the PokeAPI to provide real-time data 
+                on over 1,300+ Pokemon, featuring dynamic filtering, searching, and a detailed modal view.
+            </h4>
 
-            <section className="footerSiteDescription">
-                <section id="footerImgContainer">
-                    <img src="./public/pokedex-logo.png" alt="pokedex logo" />
-                </section>
+            <ul>
+                <li><a href="https://github.com/krobbus/PokeDex/blob/main/README.md" />
+                    <img src="./src/assets/github-logo.png" alt="github logo" />
+                </li>
 
-                <section id="footerListContainer">
-                    <h3>About PokeDex</h3>
+                <li><a href="https://www.pokemon.com/us/pokedex" />
+                    <img src="./src/assets/pokeball-icon.png" alt="pokedex logo" />
+                </li>
+            </ul>
 
-                    <ul>
-                        <li><a href="https://github.com/krobbus/PokeDex/blob/main/README.md">Learn More [Github]</a></li>
-                        <li><a href="https://www.pokemon.com/us/pokedex">Official PokeDex Site</a></li>
-                    </ul>
-                </section>
-            </section>
+            <h6>
+                &copy; 2026 by Alef Justin Loresca. Pokémon names, images, and related media are trademarks and copyrights of 
+                Nintendo, Game Freak, and Creatures.. This project is intended under "Fair Use" for educational purposes. 
+                No copyright infringement is intended
+            </h6>
         </section>
     );
 };
